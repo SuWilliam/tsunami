0e6c0ba9be6b8028cb2e419d0174ed106c200873
SuWilliam <willsu96@gmail.com>
2018-09-10 23:05:06 -0700

    added description to site

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import logo from './tsunami_logo.svg';
 import './App.css';
 
@@ -8,10 +9,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Tsunami</h1>
+          <Button>Start Game</Button>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+An unpredictable drawing game that's waves of fun!
         </p>
       </div>
     );
